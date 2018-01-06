d83f31d7326c57f02a80157756d30abbde4de1f9
Shiva Dheeraj Kamma <shiva.kamma@st.ovgu.de>
2018-01-07 00:17:07 +0100

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import {render} from "react-dom";
 import {AppComponent} from "./components/appComponent";
-//import {Slidebar} from "./components/sidebar";
 
 window.React = React;
 render(
